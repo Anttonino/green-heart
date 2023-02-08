3c8699e957c69e76482bd4b7c9cefc5598900489
Anttonino <victor.antonino12@gmail.com>
2023-02-08 15:06:07 -0300

    feat: implement the dto for user update

--- src/users/services/user.services.ts
+++ src/users/services/user.services.ts
@@ -4,72 +4,96 @@ import { Repository } from 'typeorm';
 
 import { Bcrypt } from '../../auth/bcrypt/bcrypt';
 import { User } from '../entities/users.entity';
+import { MessagesHelper } from 'src/helpers/messages.helpers';
+import { UpdateUserDto } from '../dto/updateUser.dto';
 
-@Injectable ()
+@Injectable()
 export class UserService {
-    constructor (
-        @InjectRepository (User)
-        private userRepository: Repository <User>,
-        private bcrypt: Bcrypt
-    ) { }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private bcrypt: Bcrypt,
+  ) {}
 
-    async findByUser (username: string): Promise <User> {
-        return await this.userRepository.findOne ({
-            where: {username: username}
-        })
+  async findByUser(username: string): Promise<User> {
+    return await this.userRepository.findOne({
+      where: { username: username },
+      relations: { posting: true },
+    });
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne({
+      where: { email: email },
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find({
+      relations: { posting: true },
+    });
+  }
+
+  async findById(id: string): Promise<User> {
+    let user = await this.userRepository.findOne({
+      where: { id },
+      relations: { posting: true },
+    });
+
+    if (!user)
+      throw new HttpException(
+        MessagesHelper.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
+
+    return user;
+  }
+
+  async create(data: User): Promise<User> {
+    const usernameSearch = await this.findByUser(data.username);
+
+    if (!usernameSearch) {
+      data.password = await this.bcrypt.encryptPassword(data.password);
+      const user = this.userRepository.create(data);
+      return await this.userRepository.save(user);
     }
 
-    async findByEmail (email: string): Promise <User> {
-        return await this.userRepository.findOne ({
-            where: {email: email}
-        })
-    }
+    throw new HttpException(
+      MessagesHelper.EXISTING_USERNAME,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
 
-    async findAll (): Promise <User []> {
-        return await this.userRepository.find ({
-            relations:{posting: true}
-        });
-    }
+  async update(user: UpdateUserDto): Promise<UpdateUserDto> {
+    const userUpdate: User = await this.findById(user.id);
+    const userSearch = await this.findByUser(user.username);
+    const emailSearch = await this.findByEmail(user.email);
 
-    async findById (id: number): Promise <User> {
-        let user = await this.userRepository.findOne ({
-            where: {id},
-            relations: {posting: true}
-        });
+    if (!userUpdate)
+      throw new HttpException(
+        MessagesHelper.USER_NOT_FOUND,
+        HttpStatus.NOT_FOUND,
+      );
 
-        if (!user)
-            throw new HttpException('User was not found', HttpStatus.NOT_FOUND);
+    if (userSearch && userSearch.id !== user.id)
+      throw new HttpException(
+        MessagesHelper.EXISTING_USERNAME,
+        HttpStatus.BAD_REQUEST,
+      );
 
-        return user;
-    }
+    if (emailSearch && emailSearch.id !== user.id)
+      throw new HttpException(
+        MessagesHelper.EXISTING_EMAIL,
+        HttpStatus.BAD_REQUEST,
+      );
 
-    async create (username: User): Promise <User> {
-        let searchUser = await this.findByUser(username.username);
+    this.userRepository.merge(userUpdate, user);
+    await this.userRepository.save(userUpdate);
 
-        if (!searchUser) {
-            username.password = await this.bcrypt.encryptPassword (username.password)
-            return await this.userRepository.save (username);
-        }
-        throw new HttpException ("User was not found", HttpStatus.BAD_REQUEST);
-    }
+    const updatedUser = await this.findById(user.id);
 
-    async update (username: User): Promise <User> {
+    const { password, ...userWithoutPassword } = updatedUser;
 
-        let updateUser: User = await this.findById (username.id);
-        let searchUser = await this.findByUser (username.username);
-        let searchEmail = await this.findByEmail (username.email)
-
-        if (!updateUser)
-            throw new HttpException ('User was not found', HttpStatus.NOT_FOUND);
-
-        if (searchUser && searchUser.id !== username.id)
-            throw new HttpException ('Username already exists', HttpStatus.BAD_REQUEST);
-
-        if (searchEmail && searchEmail.id !== username.id)
-            throw new HttpException ('E-mail already exists', HttpStatus.BAD_REQUEST);
-
-        username.password = await this.bcrypt.encryptPassword (username.password)
-        return await this.userRepository.save (username);
-    }
-
+    return userWithoutPassword;
+  }
 }
