5f74f17e8520c5a170dc1acef083a5b56e097c53
Anttonino <victor.antonino12@gmail.com>
2023-02-08 14:59:14 -0300

    feat: create function for query users by username

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,35 +1,53 @@
-import { Body, Controller, Get, HttpCode, Post, Put, UseGuards } from "@nestjs/common/decorators";
-import { HttpStatus } from "@nestjs/common/enums";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger/dist";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common/decorators';
+import { HttpStatus } from '@nestjs/common/enums';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger/dist';
 
-import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
-import { User } from "../entities/users.entity";
-import { UserService } from "../services/user.services";
+import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
+import { User } from '../entities/users.entity';
+import { UserService } from '../services/user.services';
+import { UpdateUserDto } from '../dto/updateUser.dto';
 
 @ApiTags('User')
-@Controller ('/users')
+@Controller('/users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-        @ApiBearerAuth()
-        @UseGuards (JwtAuthGuard)
-        @Get ('/all')
-        @HttpCode (HttpStatus.OK)
-        findAll (): Promise <User []> {
-            return this.userService.findAll ();
-        }
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('/all')
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
 
-        @Post ('/sign_up')
-        @HttpCode (HttpStatus.CREATED)
-        async create (@Body () user: User): Promise <User> {
-            return this.userService.create (user);
-        }
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('/:user')
+  @HttpCode(HttpStatus.OK)
+  findByUser(@Param('user') user: string): Promise<User> {
+    return this.userService.findByUser(user);
+  }
 
-        @ApiBearerAuth()
-        @UseGuards (JwtAuthGuard)
-        @Put ('/update')
-        @HttpCode (HttpStatus.OK)
-        async update (@Body () username: User): Promise <User> {
-            return this.userService.update (username)
-        }
+  @Post('/sign_up')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() user: User): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Put('/update')
+  @HttpCode(HttpStatus.OK)
+  async update(@Body() user: UpdateUserDto): Promise<UpdateUserDto> {
+    return this.userService.update(user);
+  }
 }
